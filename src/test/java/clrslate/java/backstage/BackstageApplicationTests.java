661e2a2d83fb0efdd13112822886eb87c0c24543
Piyush Rathi <jst4upiyush@gmail.com>
2024-07-21 03:51:41 +0530

    initial commit

---
+++ src/test/java/clrslate/java/backstage/BackstageApplicationTests.java
@@ -0,0 +1,13 @@
+package clrslate.java.backstage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackstageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
